aa095ce869c195efa16a242ed49e319030f783f8
Ulisses Dias <ulisses@ft.unicamp.br>
2021-05-25 14:46:44 -0300

    Completando o cliente da API Rest

--- lib/data/web_server/remote_database.dart
+++ lib/data/web_server/remote_database.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:aula09_2021/model/note.dart';
 import 'package:dio/dio.dart';
 
@@ -30,16 +32,43 @@ class DatabaseRemoteServer {
     return [noteList, idList];
   }
 
-  Future<int> insertNote(Note note) async {}
+  Future<int> insertNote(Note note) async {
+    await _dio.post(this.databaseUrl,
+        options: Options(headers: {"Accept": "application/json"}),
+        data:
+            jsonEncode({"title": note.title, "description": note.description}));
+    return 1;
+  }
 
-  Future<int> updateNote(int noteId, Note note) async {}
+  Future<int> updateNote(int noteId, Note note) async {
+    await _dio.put(this.databaseUrl + "/$noteId",
+        options: Options(headers: {"Accept": "application/json"}),
+        data:
+            jsonEncode({"title": note.title, "description": note.description}));
+    return 1;
+  }
 
-  Future<int> deleteNote(int noteId) async {}
+  Future<int> deleteNote(int noteId) async {
+    await _dio.delete(this.databaseUrl + "/$noteId",
+        options: Options(method: "GET", headers: {
+          "Accept": "application/json",
+        }));
+    return 1;
+  }
 }
 
 void main() async {
   DatabaseRemoteServer noteService = DatabaseRemoteServer.helper;
+  /*
   var response = await noteService.getNoteList();
   Note note = response[0][0];
   print(note.title);
+  */
+
+  Note note = Note();
+  note.title = "Gabriel Gomes";
+  note.description = "Aluno de SI700";
+  //noteService.insertNote(note);
+  //noteService.updateNote(0, note);
+  noteService.deleteNote(1);
 }
